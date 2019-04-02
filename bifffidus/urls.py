d0457589fbb110d1b6a4b4500efaa8b21eeb37ec
Azat0th <azat0th@skynet.be>
2019-04-02 17:12:43 +0200

    url updates, bifffidus is now main website

--- bifffidus/urls.py
+++ bifffidus/urls.py
@@ -2,27 +2,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bifffidus/', views.main_page,  name='main_page'),
-    path('bifffidus/movie/<int:pk>/',  views.movie_detail,  name='movie_detail'),
+    path('', views.main_page,  name='main_page'),
+    path('movie/<int:pk>/',  views.movie_detail,  name='movie_detail'),
 
-    path('bifffidus/festival/', views.festival_list,  name='festival_list'),
-    path('bifffidus/festival/<int:pk>/', views.festival_detail,  name='festival_detail'),
+    path('festival/', views.festival_list,  name='festival_list'),
+    path('festival/<int:pk>/', views.festival_detail,  name='festival_detail'),
 
-    path('bifffidus/movielist/<int:year>/<int:month>/<int:day>/', views.movie_by_date, name='movie_by_date'),
-    path('bifffidus/movielist/<int:pk>/', views.movie_by_festival, name='movie_by_festival'),
-    path('bifffidus/movie/', views.movie_list, name='movie_list'),    
+    path('movielist/<int:year>/<int:month>/<int:day>/', views.movie_by_date, name='movie_by_date'),
+    path('movielist/<int:pk>/', views.movie_by_festival, name='movie_by_festival'),
+    path('movie/', views.movie_list, name='movie_list'),    
     
-    path('bifffidus/person/', views.person_list, name='person_list'),
+    path('person/', views.person_list, name='person_list'),
     
-    path('bifffidus/person/<int:pk>', views.person_detail, name='person_detail'),
+    path('person/<int:pk>', views.person_detail, name='person_detail'),
     
-    path('bifffidus/genre/', views.genre_list, name='genre_list'),
-    path('bifffidus/genre/<int:pk>', views.genre_detail, name='genre_detail'),
+    path('genre/', views.genre_list, name='genre_list'),
+    path('genre/<int:pk>', views.genre_detail, name='genre_detail'),
     
-    path('bifffidus/country/', views.country_list, name='country_list'),
-    path('bifffidus/country/<int:pk>', views.country_detail, name='country_detail'),
+    path('country/', views.country_list, name='country_list'),
+    path('country/<int:pk>', views.country_detail, name='country_detail'),
     
-    path('bifffidus/tag/', views.tag_list, name='tag_list'),
-    path('bifffidus/tag/<int:pk>', views.tag_detail, name='tag_detail'),    
+    path('tag/', views.tag_list, name='tag_list'),
+    path('tag/<int:pk>', views.tag_detail, name='tag_detail'),    
     
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list,  name='post_list'), 
-    path('post/tag/<int:pk>/',  views.post_list_by_tag,  name='post_list_by_tag'), 
-    path('post/<int:year>/<int:month>/<int:day>/',  views.post_list_by_day,  name='post_list_by_day'), 
-    path('post/<int:pk>/',  views.post_detail,  name='post_detail'),
-    path('post/new/',  views.post_new,  name='post_new'),
-    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
-    path('drafts/', views.post_draft_list,  name='post_draft_list'),
-    path('post/<pk>/publish/',  views.post_publish,  name='post_publish'),
-    path('post/<pk>/remove',  views.post_remove,  name='post_remove'),
-    path('post/<pk>/comment',  views.add_comment_to_post,  name='add_comment_to_post'),
-    path('comment/<pk>/approve', views.comment_approve,  name='comment_approve'),
-    path('comment/<pk>/remove', views.comment_remove, name='comment_remove'),
+    path('blog/', views.post_list,  name='post_list'), 
+    path('blog/post/tag/<int:pk>/',  views.post_list_by_tag,  name='post_list_by_tag'), 
+    path('blog/post/<int:year>/<int:month>/<int:day>/',  views.post_list_by_day,  name='post_list_by_day'), 
+    path('blog/post/<int:pk>/',  views.post_detail,  name='post_detail'),
+    path('blog/post/new/',  views.post_new,  name='post_new'),
+    path('blog/post/<int:pk>/edit', views.post_edit, name='post_edit'),
+    path('blog/drafts/', views.post_draft_list,  name='post_draft_list'),
+    path('blog/post/<pk>/publish/',  views.post_publish,  name='post_publish'),
+    path('blog/post/<pk>/remove',  views.post_remove,  name='post_remove'),
+    path('blog/post/<pk>/comment',  views.add_comment_to_post,  name='add_comment_to_post'),
+    path('blog/comment/<pk>/approve', views.comment_approve,  name='comment_approve'),
+    path('blog/comment/<pk>/remove', views.comment_remove, name='comment_remove'),
 ]
